a773fc0f8369c18f68e37220fea50573463cc0f6
Daniel O <danielogunlana23@gmail.com>
2021-06-14 22:43:11 -0400

    called amrkdown file

--- utils/index.js
+++ utils/index.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util')
-
+const generateMarkdown = require('./generateMarkdown')
 const writeFileAsync = util.promisify(writeToFile);
 
 
