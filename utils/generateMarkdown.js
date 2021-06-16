10cbefc071aadcd4c5a033bbee121ba48900373d
Daniel O <danielogunlana23@gmail.com>
2021-06-16 08:52:42 -0400

    added license function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,56 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch(license) {
+    case"Apache":
+      return `![Apache Badge](https://img.shields.io/badge/License-Apache-blue)`;
+      case "GNU":
+        return `![GNU Badge](https://img.shields.io/badge/License-GNU-red)`;
+      case "ISC":
+        return `![ISC Badge](https://img.shields.io/badge/License-ISC-orange)`;
+      case "MIT":
+        return `![MIT Badge](https://img.shields.io/badge/License-MIT-green)`;
+      default:
+        return "";
+    
+  };
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  console.log(``)
+  switch (license) {
+    case "Apache":
+      return `[Apache License 2.0](https://choosealicense.com/licenses/apache-2.0/)`;
+    case "GNU":
+      return `[GNU General Public License v3.0](https://choosealicense.com/licenses/gpl-3.0/)`;
+    case "ISC":
+      return `[ISC License](https://choosealicense.com/licenses/isc/)`;
+    case "MIT":
+      return `[MIT License](https://choosealicense.com/licenses/mit/)`;
+    default:
+      return "";
+  };
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  console.log(license);
+  switch (license) {
+    case "Apache":
+      return `The license for this project is ${renderLicenseLink(data)}`;
+    case "GNU":
+      return `The license for this project is ${renderLicenseLink(data)}`;
+    case "ISC":
+      return `The license for this project is ${renderLicenseLink(data)}`;
+    case "MIT":
+      return `The license for this project is ${renderLicenseLink(data)}`;
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
